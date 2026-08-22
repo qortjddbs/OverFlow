010300cf6e4dac9e597d76219e6d511b42d84b3d
qortjddbs <117032925+qortjddbs@users.noreply.github.com>
2026-08-22 15:30:04 +0900

    몬스터 스폰 패킷 추가

--- OverflowJOLJAK/Shared/Protocol.h
+++ OverflowJOLJAK/Shared/Protocol.h
@@ -1,4 +1,4 @@
-﻿// 26. 07. 31 최신화
+﻿// 26. 08. 22 최신화
 #pragma once
 
 // 헤더: 패킷 전체 크기(size) + 패킷 종류(type) 모든 패킷은 이 헤더로 시작
@@ -27,9 +27,17 @@ struct sc_packet_position : PACKET_HEADER       // 3 + 16바이트
     float m_x, m_y, m_z;
 };
 
-struct sc_packet_remove_player : PACKET_HEADER
+struct sc_packet_remove_player : PACKET_HEADER      // 3 + 4바이트
 {
     int m_id;
+};
+
+struct sc_packet_monster_spawn : PACKET_HEADER      // 3 + 21바이트
+{
+    int m_id;
+    unsigned char m_monster_type;
+    float m_x, m_y, m_z;
+    int m_hp;
 };
 #pragma pack(pop)   // 여기까지만 적용
 
@@ -38,5 +46,6 @@ enum PACKET_TYPE : unsigned char    // 네트워크를 통해 밖으로 나가�
     PKT_C2S_MOVE = 1,
     PKT_S2C_ADD_PLAYER = 2,
     PKT_S2C_POSITION = 3,
-    PKT_S2C_REMOVE_PLAYER = 4
+    PKT_S2C_REMOVE_PLAYER = 4,
+    PKT_S2C_MONSTER_SPAWN = 5
 };
